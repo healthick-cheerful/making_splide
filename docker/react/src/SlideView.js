13789b30b6552a18d04fc3309a5f86f249675d16
healthick <healthick.cheerful@gmail.com>
2022-12-22 13:46:47 +0900

    start development

---
+++ docker/react/src/SlideView.js
@@ -0,0 +1,12 @@
+import react from "react"
+import '@splidejs/react-splide/css';
+
+function SlideView() {
+    return (
+        <div className="slide-view">
+
+        </div>
+    )
+}
+
+export default SlideView
---
+++ docker/react/src/Wrapper.js
@@ -0,0 +1,12 @@
+import react from "react"
+import SlideView from "./SlideView"
+
+function Wrapper() {
+    return (
+        <div className="wrapper">
+            <SlideView />
+        </div>
+    )
+}
+
+export default Wrapper
